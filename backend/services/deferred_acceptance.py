9e9cec1336861095e8235a769b6b4f46b947a18b
Drew Beckmen <drew.beckmen@yale.edu>
2024-04-09 14:23:14 -0400

    da implementation provided via package that does hospital/resident match

--- backend/services/deferred_acceptance.py
+++ backend/services/deferred_acceptance.py
@@ -1,6 +1,39 @@
 """
     This module contains the code related to outputting the results of the deferred acceptance algorithm.
     Schema of data passed to algorithm inspired by MatchingTools API (which is no longer functional): https://matchingtools.com/swagger-ui/#!/HRI/hri_demo 
+    Leveraging the matching python package
 """
+import json
+from matching.games import HospitalResident
+from matching import MultipleMatching
 
-practice_game_setup = {}
+
+def solve_matching(data: dict) -> MultipleMatching:
+    game = HospitalResident.create_from_dictionaries(
+        data["student_prefs"], data["college_prefs"], data["college_capacity"]
+    )
+    solution = game.solve(optimal="resident")
+    return solution
+
+# ======== TEST PROGRAM TO VALIDATE THE PACKAGE'S IMPLEMENTATION ===========
+def main():
+    # Read in the data
+    with open("../data/matching_api_payload.json", "r") as f:
+        data = json.load(f)
+
+    # Create the game
+    game = HospitalResident.create_from_dictionaries(
+        data["student_prefs"], data["college_prefs"], data["college_capacity"]
+    )
+    solution = game.solve(optimal="resident")
+
+    # Check status
+    print(game.check_stability(), game.check_validity())
+
+    # Print the results
+    for school, students in solution.items():
+        print(f"{school} (Admitted Students {len(students)} / Capacity {school.capacity}): Student IDs {students}")
+
+if __name__ == "__main__":
+    main()
+# ===========================================================================
